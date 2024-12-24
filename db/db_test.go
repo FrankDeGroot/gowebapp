94e48ef80a59f52ec4e06ed46e332f3aa6b1c0aa
FrankDeGroot <fjtdg@hotmail.com>
2024-12-24 17:04:53 +0000

    + test GetAll

--- db/db_test.go
+++ db/db_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"testing"
+	"time"
 	"todo-app/dto"
 )
 
@@ -9,7 +10,7 @@ func TestInsertDelete(t *testing.T) {
 	Connect()
 	defer Close()
 
-	toDo := dto.ToDo{Description: "Test", Done: true}
+	toDo := dto.ToDo{Description: "Test" + time.Now().Format(time.RFC3339), Done: true}
 	savedToDo, err := Insert(&toDo)
 	if err != nil {
 		t.Errorf("Error Inserting To Do %v", err)
@@ -23,13 +24,33 @@ func TestInsertDelete(t *testing.T) {
 
 	foundToDo, err := GetOne(savedToDo.Id)
 	if err != nil {
-		t.Errorf("Error finding To Do %v", savedToDo)
+		t.Errorf("Error finding To Do %v", err)
 	}
 	if foundToDo.Description != toDo.Description {
 		t.Errorf("Wanted %v got %v", toDo.Description, savedToDo.Description)
 	}
 	if foundToDo.Done != toDo.Done {
 		t.Errorf("Wanted %v got %v", toDo.Done, savedToDo.Done)
+	}
+
+	toDos, err := GetAll()
+	found := false
+	if err != nil {
+		t.Errorf("Error finding To Dos %v", err)
+	}
+	for _, todo := range *toDos {
+		if todo.Id == savedToDo.Id {
+			found = true
+			if todo.Description != toDo.Description {
+				t.Errorf("Wanted %v got %v", toDo.Description, todo.Description)
+			}
+			if todo.Done != toDo.Done {
+				t.Errorf("Wanted %v got %v", toDo.Done, todo.Done)
+			}
+		}
+	}
+	if !found {
+		t.Errorf("To Do not found in GetAll")
 	}
 
 	err = Delete(savedToDo.Id)
