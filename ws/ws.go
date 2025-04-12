1a783dafb541b9eb545e73741ecf7a7881bb6233
FrankDeGroot <fjtdg@hotmail.com>
2025-04-12 21:18:19 +0000

    fix ws and add test

--- ws/ws.go
+++ ws/ws.go
@@ -4,21 +4,22 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"time"
 	"todo-app/act"
 
 	"github.com/coder/websocket"
 	"github.com/coder/websocket/wsjson"
 )
 
+const WS_TODO_PATH = "/ws/todos"
+
 var (
-	conns = make(map[*websocket.Conn]struct{}, 0)
-	prod  Producer
+	prod     Producer
+	connChan = make(chan *websocket.Conn)
 )
 
 func Init(p Producer, c Consumer) act.Notifier {
 	prod = p
-	http.HandleFunc("GET /ws/todos", getToDoActions)
+	http.HandleFunc("GET "+WS_TODO_PATH, getToDoActions)
 	go consume(c)
 	return notify
 }
@@ -31,27 +32,39 @@ func notify(todoAction *act.TodoAction) {
 }
 
 func consume(cons Consumer) {
+	conns := make(map[*websocket.Conn]struct{}, 0)
 	for {
-		if len(conns) == 0 {
-			time.Sleep(time.Second)
-			continue
-		}
-		todo, err := cons.Consume()
-		if err != nil {
-			log.Printf("Error consuming todo: %v\n", err)
-		}
-		if todo == nil {
-			continue
-		}
-		for conn := range conns {
-			log.Printf("Sending todo event")
-			err = wsjson.Write(context.Background(), conn, todo)
+		conn := <-connChan
+		conns[conn] = struct{}{}
+
+		for len(conns) != 0 {
+			todo, err := cons.Consume()
 			if err != nil {
-				delete(conns, conn)
-				log.Printf("Error writing to socket: %v", err)
+				log.Printf("Error consuming todo: %v\n", err)
 			}
+			if todo == nil {
+				continue
+			}
+
+		AddConns:
+			for {
+				select {
+				case conn := <-connChan:
+					conns[conn] = struct{}{}
+				default:
+					break AddConns
+				}
+			}
+			for conn := range conns {
+				log.Printf("Sending todo event")
+				err = wsjson.Write(context.Background(), conn, todo)
+				if err != nil {
+					delete(conns, conn)
+					log.Printf("Error writing to socket: %v", err)
+				}
+			}
+			log.Printf("Sent events successfully to %v connections", len(conns))
 		}
-		log.Printf("Sent events successfully to %v connections", len(conns))
 	}
 }
 
@@ -62,6 +75,5 @@ func getToDoActions(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error", http.StatusInternalServerError)
 		log.Printf("Error accepting websocket: %v\n", err)
 	}
-	conns[c] = struct{}{}
-	log.Printf("Websocket connected %v connections", len(conns))
+	connChan <- c
 }
--- ws/ws_test.go
+++ ws/ws_test.go
@@ -1,8 +1,16 @@
 package ws
 
 import (
+	"net/http/httptest"
+	"strings"
 	"testing"
+	"todo-app/act"
+	"todo-app/dto"
 	"todo-app/ws/mocks"
+
+	"github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
+	"github.com/stretchr/testify/assert"
 )
 
 var (
@@ -10,8 +18,26 @@ var (
 	mockConsumer = new(mocks.MockConsumer)
 )
 
-func TestInit(t *testing.T) {
-	if Init(mockProducer, mockConsumer) == nil {
-		t.Fatal("Init should not return nil")
-	}
+func TestWebSocketConnection(t *testing.T) {
+	assert.NotNil(t, Init(mockProducer, mockConsumer))
+	srv := httptest.NewServer(nil)
+	defer srv.Close()
+
+	todo := &act.TodoAction{Action: act.Add,
+		SavedTodo: dto.SavedTodo{Id: "123",
+			Todo: dto.Todo{Description: "123", Done: false}}}
+	mockConsumer.On("Consume").Return(todo, nil)
+
+	url := "ws" + strings.TrimPrefix(srv.URL, "http") + WS_TODO_PATH
+	t.Log(url)
+	c, _, err := websocket.Dial(t.Context(), url, nil)
+	assert.NoError(t, err)
+	defer c.CloseNow()
+
+	recvTodo := &act.TodoAction{}
+	err = wsjson.Read(t.Context(), c, recvTodo)
+	assert.NoError(t, err)
+	assert.NotNil(t, recvTodo)
+	assert.Equal(t, todo.Id, recvTodo.Id)
+	assert.Equal(t, todo.Description, recvTodo.Description)
 }
