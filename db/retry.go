14271d415944d33a5be1b6aa0cbc099d656406a5
FrankDeGroot <fjtdg@hotmail.com>
2025-04-25 19:58:13 +0000

    rename jitter

--- db/retry.go
+++ db/retry.go
@@ -13,8 +13,8 @@ func retry[T any](attempts int, duration time.Duration, f func() (T, error)) (re
 	for i := 0; i < attempts; i++ {
 		if i > 0 {
 			log.Println("retrying after error:", err)
-			randAdjustment := time.Duration(rand.Int63n(int64(duration)/5)) - duration/10
-			time.Sleep(duration + randAdjustment)
+			jitter := time.Duration(rand.Int63n(int64(duration)/5)) - duration/10
+			time.Sleep(duration + jitter)
 			duration *= 2
 		}
 		result, err = f()
