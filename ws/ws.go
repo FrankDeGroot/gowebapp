818ba21e676c4a15e338c2335b08685fb806cc78
FrankDeGroot <fjtdg@hotmail.com>
2025-05-03 20:38:49 +0000

    Add reading from websockets Fixes #7

--- ws/ws.go
+++ ws/ws.go
@@ -19,8 +19,8 @@ var (
 
 func Open(p Producer, c Consumer) act.Notifier {
 	prod = p
-	http.HandleFunc("GET "+WS_PATH, wsConnect)
-	go consume(c)
+	http.HandleFunc("GET "+WS_PATH, connect)
+	go broadcast(c)
 	return notify
 }
 
@@ -35,12 +35,54 @@ func notify(taskAction *act.TaskAction) {
 	}
 }
 
-func consume(cons Consumer) {
+func broadcast(cons Consumer) {
 	conns := make(map[*websocket.Conn]struct{}, 0)
-	for conn := range connChan {
-		conns[conn] = struct{}{}
+	consChan := make(chan *act.TaskAction)
+	defer close(consChan)
+	consContChan := make(chan bool)
+	defer close(consContChan)
+	for {
+		select {
+		case conn, ok := <-connChan:
+			if !ok {
+				return
+			}
+			conns[conn] = struct{}{}
+			readContChan := make(chan struct{})
+			defer close(readContChan)
+			go read(conn, readContChan)
+			if len(conns) == 1 {
+				go consume(cons, consChan, consContChan)
+			}
+		case task, ok := <-consChan:
+			if !ok {
+				return
+			}
+			log.Printf("Broadcasting %v to %v conns", task, len(conns))
+			for conn := range conns {
+				err := wsjson.Write(context.Background(), conn, task)
+				if err != nil {
+					conn.CloseNow()
+					delete(conns, conn)
+					log.Printf("Error writing to socket: %v", err)
+					if len(conns) == 0 {
+						consContChan <- false
+					}
+				}
+			}
+		}
+	}
+}
 
-		for len(conns) != 0 {
+func consume(cons Consumer, consChan chan *act.TaskAction, contChan chan bool) {
+	cont, ok := true, true
+	for {
+		select {
+		case cont, ok = <-contChan:
+			if !cont || !ok {
+				return
+			}
+		default:
 			task, err := cons.Consume()
 			if err != nil {
 				log.Printf("Error consuming task: %v\n", err)
@@ -48,38 +90,38 @@ func consume(cons Consumer) {
 			if task == nil {
 				continue
 			}
-
-		addConns:
-			for {
-				select {
-				case conn, ok := <-connChan:
-					if !ok {
-						return
-					}
-					conns[conn] = struct{}{}
-				default:
-					break addConns
-				}
-			}
-			for conn := range conns {
-				log.Printf("Sending task event")
-				err = wsjson.Write(context.Background(), conn, task)
-				if err != nil {
-					delete(conns, conn)
-					log.Printf("Error writing to socket: %v", err)
-				}
-			}
-			log.Printf("Sent events successfully to %v connections", len(conns))
+			log.Printf("Consumed %v", task)
+			consChan <- task
 		}
 	}
 }
 
-func wsConnect(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Getting events")
-	c, err := websocket.Accept(w, r, nil)
+func read(conn *websocket.Conn, cont chan struct{}) {
+	for {
+		select {
+		case _, ok := <-cont:
+			if !ok {
+				return
+			}
+		default:
+			action := act.TaskAction{}
+			err := wsjson.Read(context.Background(), conn, &action)
+			if err != nil {
+				log.Printf("Error reading websocket %v", err)
+				conn.CloseNow()
+				return
+			}
+			log.Printf("Read %v", action)
+		}
+	}
+}
+
+func connect(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Connect")
+	conn, err := websocket.Accept(w, r, nil)
 	if err != nil {
 		http.Error(w, "Error", http.StatusInternalServerError)
-		log.Printf("Error accepting websocket: %v\n", err)
+		log.Printf("Error accepting websocket: %v", err)
 	}
-	connChan <- c
+	connChan <- conn
 }
